b4aee8cf4e0ccf722983807462fa715507438380
Sardhar5599 <sardharhussain1234@gmail.com>
2024-07-12 14:08:40 +0530

    Update Allproducts.js

--- src/pages/Allproducts.js
+++ src/pages/Allproducts.js
@@ -18,39 +18,39 @@ function Allproducts({cart,setcart}) {
   const products = [
     {
         id:'1',
-      image: 'https://images.unsplash.com/photo-1587049352851-8d4e89133924?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-      description: 'Derived organic honey',
-      title: 'Rose Honey'
+      image: 'https://mir-s3-cdn-cf.behance.net/project_modules/fs/4fd83999831295.5efb78b2b5cb3.jpg',
+      description: 'Villa',
+      title: 'Villa'
     },
     {
         id:'2',
-      image: 'https://images.unsplash.com/photo-1576086213369-97d6cf3a937c?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-      description: 'Pure wildflower honey',
-      title: 'Wildflower Honey'
+      image: 'https://foyr.com/learn/wp-content/uploads/2019/03/best-home-garden-ideas-1024x699.png',
+      description: 'Garden',
+      title: 'Garden'
     },
     {
         id:'3',
-      image: 'https://images.unsplash.com/photo-1578985545062-69928b1d9587?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-      description: 'Raw acacia honey',
-      title: 'Acacia Honey'
+      image: 'https://images.homedepot-static.com/lifestyleimages/640x426/8b784421-0ece-435b-a56b-022f29409bad4.jpeg',
+      description: 'Bath Room',
+      title: 'Bath Room'
     },
     {
         id:'4',
-      image: 'https://images.unsplash.com/photo-1598378678822-45c2bde0a281?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-      description: 'Organic lavender honey',
-      title: 'Lavender Honey'
+      image: 'https://www.milindpai.com/wp-content/uploads/2023/03/maxresdefault.jpg',
+      description: 'Bed Room',
+      title: 'Bed Room'
     },
     {
         id:'5',
-      image: 'https://images.unsplash.com/photo-1598378678663-720a7e6f162f?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-      description: 'Forest honey',
-      title: 'Forest Honey'
+      image: 'https://hips.hearstapps.com/hmg-prod/images/built-in-refrigerators-1557866621.png',
+      description: 'Kitchen',
+      title: 'Kitchen'
     },
     {
         id:'6',
-      image: 'https://images.unsplash.com/photo-1571689937702-87918c28e7c8?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-      description: 'Manuka honey',
-      title: 'Manuka Honey'
+      image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvHGWj-LcvigEm8VpH5B1JZci-NOIZipxJmw&usqp=CAU',
+      description: 'Hall',
+      title: 'Hall'
     }
   ];
 
